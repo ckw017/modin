d1b88adace9b691501fff72743043605d27c8ed6
Chris Wong <chriskw.xyz@gmail.com>
2021-06-04 16:17:39 -0700

    add test ray client job
    Signed-off-by: Chris Wong <chriskw.xyz@gmail.com>

--- modin/conftest.py
+++ modin/conftest.py
@@ -42,7 +42,7 @@ modin.utils._make_api_url = _saving_make_api_url
 
 import modin  # noqa: E402
 import modin.config  # noqa: E402
-from modin.config import IsExperimental  # noqa: E402
+from modin.config import IsExperimental, TestRayClient  # noqa: E402
 
 from modin.backends import PandasQueryCompiler, BaseQueryCompiler  # noqa: E402
 from modin.engines.python.pandas_on_python.io import PandasOnPythonIO  # noqa: E402
@@ -452,3 +452,34 @@ def TestReadGlobCSVFixture():
 @pytest.fixture
 def get_generated_doc_urls():
     return lambda: _generated_doc_urls
+
+
+def pytest_sessionstart(session):
+    if TestRayClient.get():
+        import ray
+        import subprocess
+        from packaging import version
+        if version.parse(ray.__version__) <= version.parse("1.3.0"):
+            from ray.util.client.common import ClientBaseRef, ClientObjectRef
+            # This fixes an issue in ray 1.3 that will be resolved in the 1.4
+            # release (https://github.com/ray-project/ray/pull/15320)
+            # Can be removed once the ray version for the ray client tests is
+            # bumped to 1.4
+
+            def patched_eq(self, other):
+                return isinstance(other, ClientBaseRef) and self.id == other.id
+            ClientObjectRef.__eq__ = patched_eq
+
+        port = '50051'
+        # Clean up any extra processes from previous runs
+        subprocess.check_output(["ray", "stop", "--force"])
+        subprocess.check_output(["ray", "start", "--head", "--num-cpus", "2", "--ray-client-server-port", port])
+        ray.util.connect(f"0.0.0.0:{port}")
+
+
+def pytest_sessionfinish(session):
+    if TestRayClient.get():
+        import ray
+        import subprocess
+        ray.shutdown()
+        subprocess.check_output(["ray", "stop", "--force"])
